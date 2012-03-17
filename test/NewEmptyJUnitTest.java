c9528072ae5ffb07217d74a8477efb49c52a2e13
lizandro <lizandro@lizandro-PC>
2012-03-17 13:58:35 +0100

    Testing Git.

---
+++ test/NewEmptyJUnitTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lizandro
+ */
+public class NewEmptyJUnitTest {
+   
+   public NewEmptyJUnitTest() {
+   }
+   @BeforeClass
+   public static void setUpClass() throws Exception {
+   }
+   @AfterClass
+   public static void tearDownClass() throws Exception {
+   }
+   @Before
+   public void setUp() {
+   }
+   @After
+   public void tearDown() {
+   }
+}
